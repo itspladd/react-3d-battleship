9bd49e0a7c10547fcb743bf59c3817fe3c3ba11f
Paul Ladd <vlendle@gmail.com>
2021-07-02 15:50:22 -0500

    Add semi-final testing and functionality for boardCoordinatesToSceneCoordinates

--- src/helpers/boardHelpers.js
+++ src/helpers/boardHelpers.js
@@ -11,8 +11,21 @@ const boardCoordinatesToSceneCoordinates = function ({
   // If no x/y offset given, set them to 0
   xOffset = xOffset || 0;
   yOffset = yOffset || 0;
+  let newX = 0;
+  let newY = 0;
 
-  return [0, 0]
+  // Horizontal distance from center of one tile to center of next
+  const xOffsetPerTile = 1.5 * tileRadius;
+  // Vertical distance from center of one tile to center of next
+  const yOffsetPerTile = 2 * tileHeight;
+  // Extra vertical offset for odd-numbered columns;
+  const oddColumnOffset = yOffsetPerTile / 2;
+
+  newX = x * xOffsetPerTile;
+  // If the column is odd (x%2 = 1), add the extra offset to the Y
+  newY = y * yOffsetPerTile + ((x % 2) * oddColumnOffset);
+
+  return [newX, newY]
 }
 
 module.exports = {
--- src/tests/boardHelpers.test.js
+++ src/tests/boardHelpers.test.js
@@ -13,5 +13,27 @@ describe('boardCoordinatesToSceneCoordinates', () => {
     const [resultX, resultY] = boardCoordinatesToSceneCoordinates(parameters)
     expect(resultX).toEqual(0)
     expect(resultY).toEqual(0)
+  });
+  it('should return 1.5, 1 when given 1, 0', () => {
+    const parameters = {
+      x: 1,
+      y: 0,
+      tileRadius: 1,
+      tileHeight: 1
+    }
+    const [resultX, resultY] = boardCoordinatesToSceneCoordinates(parameters)
+    expect(resultX).toEqual(1.5)
+    expect(resultY).toEqual(1)
+  });
+  it('should return 7.5, 11 when given 5, 5', () => {
+    const parameters = {
+      x: 5,
+      y: 5,
+      tileRadius: 1,
+      tileHeight: 1
+    }
+    const [resultX, resultY] = boardCoordinatesToSceneCoordinates(parameters)
+    expect(resultX).toEqual(7.5)
+    expect(resultY).toEqual(11)
   })
 })
